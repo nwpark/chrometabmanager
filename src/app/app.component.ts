5e9bcea0954a45eb37ded9d14bf091dd4da0160d
Nick Park <nick.wpa@gmail.com>
2019-11-19 17:15:38 +0000

    made column layout responsive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  static readonly BREAKPOINT_M = 1400;
+  static readonly BREAKPOINT_S = 900;
+
   title = 'tabmanager';
+  cols: number;
+
+  ngOnInit(): void {
+    this.cols = this.getCols(window.innerWidth);
+  }
+
+  onResize(event) {
+    this.cols = this.getCols(event.target.innerWidth);
+  }
+
+  getCols(windowWidth: number) {
+    if (window.innerWidth > AppComponent.BREAKPOINT_M) {
+      return 3;
+    }
+    if (window.innerWidth > AppComponent.BREAKPOINT_S) {
+      return 2;
+    }
+    return 1;
+  }
+
 }
