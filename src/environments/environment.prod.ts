300761c44b56379dfb758fb0ef35d965f2a63bcc
Nick Park <nick.wpa@gmail.com>
2019-12-23 12:17:40 +0000

    update background image

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   backgroundPhotoMetadata: {
-    author: 'Stas Bartnikas',
-    url: 'https://www.flickr.com/photos/100799973@N07/33167804003/in/gallery-152977080@N03-72157680462443671/'
+    author: 'Owen Perry',
+    url: 'https://www.flickr.com/photos/152977080@N03/34668462710/'
   }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,8 @@
 export const environment = {
   production: false,
   backgroundPhotoMetadata: {
-    author: 'Stas Bartnikas',
-    url: 'https://www.flickr.com/photos/100799973@N07/33167804003/in/gallery-152977080@N03-72157680462443671/'
+    author: 'Owen Perry',
+    url: 'https://www.flickr.com/photos/152977080@N03/34668462710/'
   }
 };
 
