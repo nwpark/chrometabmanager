4707cd655f7635d3734616cfccecd76047d2d763
Nick Park <nick.wpa@gmail.com>
2020-05-30 21:39:12 +0100

    add context menu to chrome tabs

--- src/app/components/draggable-chrome-tab/draggable-chrome-tab.component.ts
+++ src/app/components/draggable-chrome-tab/draggable-chrome-tab.component.ts
@@ -1,11 +1,14 @@
-import {ChangeDetectorRef, Component, Input, OnInit} from '@angular/core';
+import {ChangeDetectorRef, Component, Input, OnInit, ViewChild, ViewContainerRef} from '@angular/core';
 import {AnimationEvent, transition, trigger, useAnimation} from '@angular/animations';
 import {AnimationState, closeTabAnimation} from '../../animations';
-import {SessionComponentProps} from '../../types/chrome-window-component-data';
+import {SessionComponentProps, SessionListId} from '../../types/chrome-window-component-data';
 import {DomSanitizer, SafeUrl} from '@angular/platform-browser';
 import {ChromeAPITabState, getUrl, hasTitle} from '../../types/chrome-api/chrome-api-tab-state';
 import {WebpageTitleCacheService} from '../../services/webpage-title-cache.service';
 import {environment} from '../../../environments/environment';
+import {ContextMenuService} from '../../services/context-menu.service';
+import {EditableTextComponent} from '../editable-text/editable-text.component';
+import {ContextMenuItem} from '../../types/action-bar/context-menu-item';
 
 @Component({
   selector: 'app-draggable-chrome-tab',
@@ -25,13 +28,17 @@ export class DraggableChromeTabComponent implements OnInit {
   @Input() props: SessionComponentProps;
   @Input() parentIndex: number;
 
-  animationState = AnimationState.Complete;
+  @ViewChild(EditableTextComponent, {static: false}) titleTextComponent: EditableTextComponent;
+
   title: string;
   faviconIconUrl: SafeUrl;
+  animationState = AnimationState.Complete;
 
   constructor(private webpageTitleCacheService: WebpageTitleCacheService,
+              private contextMenuService: ContextMenuService,
               private changeDetectorRef: ChangeDetectorRef,
-              private domSanitizer: DomSanitizer) { }
+              private domSanitizer: DomSanitizer,
+              private viewContainerRef: ViewContainerRef) { }
 
   ngOnInit() {
     this.title = hasTitle(this.chromeTab)
@@ -54,8 +61,10 @@ export class DraggableChromeTabComponent implements OnInit {
     return this.chromeTab.status === 'loading';
   }
 
-  setTabActive(event: MouseEvent) {
-    this.props.tabsService.setTabActive(this.chromeTab, event.ctrlKey);
+  setTabActive(openInNewTab: boolean) {
+    if (!this.titleTextComponent.isEditing) {
+      this.props.tabsService.setTabActive(this.chromeTab, openInNewTab);
+    }
   }
 
   closeTab() {
@@ -65,6 +74,34 @@ export class DraggableChromeTabComponent implements OnInit {
   completeCloseAnimation(event: AnimationEvent) {
     if (event.toState === AnimationState.Closing) {
       this.props.tabsService.removeTab(this.parentIndex, this.chromeTab.id);
+    }
+  }
+
+  setTitle(title: string) {
+    // todo
+  }
+
+  showEditTitleForm() {
+    this.titleTextComponent.showEditForm();
+  }
+
+  openContextMenu(event: MouseEvent) {
+    this.contextMenuService.openContextMenu(event, this.getContextMenuItems(), this.viewContainerRef);
+  }
+
+  private getContextMenuItems(): ContextMenuItem[] {
+    switch (this.props.sessionListId) {
+      case SessionListId.Saved: return [
+        {title: 'Edit title', icon: 'edit', tooltip: 'Edit title', callback: () => this.showEditTitleForm()},
+        {title: 'Open', icon: 'open_in_new', tooltip: 'Open tab', callback: () => this.setTabActive(false)},
+        {title: 'Delete', icon: 'delete', tooltip: 'Delete saved tab', callback: () => this.closeTab()}
+      ];
+      case SessionListId.Active: return [
+        {title: 'Open', icon: 'open_in_new', tooltip: 'Open tab', callback: () => this.setTabActive(false)},
+        // todo: implement callback
+        {title: 'Suspend', icon: 'pause_circle_filled', tooltip: 'Suspend tab to free up memory and CPU consumed by chrome', callback: () => {}},
+        {title: 'Close', icon: 'close', tooltip: 'Close tab', callback: () => this.closeTab()}
+      ];
     }
   }
 }
--- src/app/services/context-menu.service.ts
+++ src/app/services/context-menu.service.ts
@@ -1,5 +1,6 @@
 import {ComponentFactoryResolver, Injectable, ViewContainerRef} from '@angular/core';
 import {ContextMenuComponent} from '../components/context-menu/context-menu.component';
+import {ContextMenuItem} from '../types/action-bar/context-menu-item';
 
 @Injectable({
   providedIn: 'root'
@@ -8,11 +9,12 @@ export class ContextMenuService {
 
   constructor(private componentFactoryResolver: ComponentFactoryResolver) { }
 
-  openContextMenu(event: MouseEvent, viewContainerRef: ViewContainerRef) {
+  openContextMenu(event: MouseEvent, menuItems: ContextMenuItem[], viewContainerRef: ViewContainerRef) {
     event.preventDefault();
     const componentFactory = this.componentFactoryResolver.resolveComponentFactory(ContextMenuComponent);
     const componentRef = viewContainerRef.createComponent(componentFactory);
     componentRef.instance.setPosition(event.clientX, event.clientY);
+    componentRef.instance.menuItems = menuItems;
     componentRef.instance.contextMenuClosed.subscribe(() => {
       componentRef.destroy();
     });
