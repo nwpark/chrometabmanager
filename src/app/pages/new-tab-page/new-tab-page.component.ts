8e21156ab84182b5529e56877f25891be4232f7a
Nick Park <nick.wpa@gmail.com>
2020-06-02 01:09:52 +0100

    move logic from new tab page component to session list components

--- src/app/pages/new-tab-page/new-tab-page.component.ts
+++ src/app/pages/new-tab-page/new-tab-page.component.ts
@@ -1,14 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {animate, state, style, transition, trigger} from '@angular/animations';
-import {SessionComponentProps, SessionListId} from '../../types/chrome-window-component-data';
-import {ChromeTabsService} from '../../services/tabs/chrome-tabs.service';
-import {SavedTabsService} from '../../services/tabs/saved-tabs.service';
-import {RecentlyClosedTabsService} from '../../services/tabs/recently-closed-tabs.service';
 import {environment} from '../../../environments/environment';
 import {ImageData} from '../../types/image-data';
-import {SessionListState} from '../../types/session/session-list-state';
-import {Observable} from 'rxjs';
-import {DragDropService} from '../../services/drag-drop.service';
 
 @Component({
   selector: 'app-new-tab-page',
@@ -35,48 +28,11 @@ export class NewTabPageComponent implements OnInit {
   cols: number;
   backgroundPhoto: ImageData;
 
-  // todo: separate component for each
-  activeSessionState$: Observable<SessionListState>;
-  savedSessionState$: Observable<SessionListState>;
-  closedSessionState$: Observable<SessionListState>;
-
-  activeSessionProps: SessionComponentProps;
-  savedSessionProps: SessionComponentProps;
-  recentlyClosedSessionProps: SessionComponentProps;
-
-  constructor(private chromeTabsService: ChromeTabsService,
-              private savedTabsService: SavedTabsService,
-              private recentlyClosedTabsService: RecentlyClosedTabsService,
-              private dragDropService: DragDropService) { }
+  constructor() { }
 
   ngOnInit(): void {
     this.cols = this.getCols(window.innerWidth);
     this.backgroundPhoto = environment.backgroundPhoto;
-    this.activeSessionProps = {
-      sessionListId: SessionListId.Active,
-      tabsService: this.chromeTabsService,
-      isMutable: true
-    };
-    this.savedSessionProps = {
-      sessionListId: SessionListId.Saved,
-      tabsService: this.savedTabsService,
-      isMutable: true
-    };
-    this.recentlyClosedSessionProps = {
-      sessionListId: SessionListId.RecentlyClosed,
-      tabsService: this.recentlyClosedTabsService,
-      isMutable: false
-    };
-    this.activeSessionState$ = this.chromeTabsService.sessionStateUpdated$.pipe(
-      // todo: create more abstract service to prevent updates
-      this.dragDropService.ignoreWhenDragging()
-    );
-    this.savedSessionState$ = this.savedTabsService.sessionStateUpdated$.pipe(
-      this.dragDropService.ignoreWhenDragging()
-    );
-    this.closedSessionState$ = this.recentlyClosedTabsService.sessionStateUpdated$.pipe(
-      this.dragDropService.ignoreWhenDragging()
-    );
   }
 
   onResize(event) {
